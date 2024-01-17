203d29ef922c8ea4cea07712329d2090909d1029
Lennard Harteveld <s1145074@student.hsleiden.nl>
2024-01-17 12:09:03 +0100

    test

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleInfo(clickedEvent) {
+    var events = document.querySelectorAll('.event');
+    
+    events.forEach(function(event) {
+      var info = event.querySelector('.more-info');
+      if (event === clickedEvent) {
+        info.style.display = info.style.display === 'none' ? 'block' : 'none';
+      } else {
+        info.style.display = 'none';
+      }
+    });
+}
